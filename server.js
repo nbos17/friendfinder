8a95ed978442e5e005dde59b5c391232b111b6bc
Your Name <nbos0531@gmail.com>
2018-03-02 22:15:14 -0600

    finish1

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.use(bodyParser.json());
 
 
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 
 
